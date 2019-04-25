7ef7c0e8ef9813723ada9b4e3506a21ed1719458
Ahmzyjazzy <olanrewajuahmed095@yahoo.com>
2019-04-25 19:41:33 +0100

    test

---
+++ client/build/precache-manifest.d929e942c7ff46cf5d3cef4db4ccd53b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9238d5d15c32fa709df",
+    "url": "/static/css/main.f4fe45b4.chunk.css"
+  },
+  {
+    "revision": "b9238d5d15c32fa709df",
+    "url": "/static/js/main.b9238d5d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ddd5cd540228d511b35f",
+    "url": "/static/css/2.bf84c92f.chunk.css"
+  },
+  {
+    "revision": "ddd5cd540228d511b35f",
+    "url": "/static/js/2.ddd5cd54.chunk.js"
+  },
+  {
+    "revision": "5300322f5a86f895c106cee849dbffaa",
+    "url": "/static/media/logo-acada-black.5300322f.svg"
+  },
+  {
+    "revision": "b2b95adc25098fddd3b2bda82204889c",
+    "url": "/index.html"
+  }
+];
