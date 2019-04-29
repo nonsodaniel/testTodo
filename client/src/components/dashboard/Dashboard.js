95d218c9f655b136e47c16ebe68e08aa150f7226
Ahmzyjazzy <olanrewajuahmed095@yahoo.com>
2019-04-29 20:45:33 +0100

    hello

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -12,7 +12,7 @@ class Dashboard extends Component {
   componentDidMount() {
     let isDev = /localhost/.test(window.location.origin);
     console.log("isdev", isDev)
-    let base_url = isDev ? "http://localhost:4000/api" : "http://acadanews.herokuapp.com/api"
+    let base_url = isDev ? "http://localhost:4000/api" : "https://acadanews.herokuapp.com/api"
 
     fetch(`${base_url}/admin/`).then((res) => {
       return res.json();
